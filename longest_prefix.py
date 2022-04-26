293528818b5876e5956f0a7eb8461b784aa53f19
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-04-26 17:59:28 +0530

    Added process

--- longest_prefix.py
+++ longest_prefix.py
@@ -9,17 +9,48 @@
 # Input: strs = ["flower","flow","flight"]
 # Output: "fl"
 
+class LongestPrefix:
+   def __init__(self, inpt_string:str) -> None:
+      self.input_string_list = inpt_string.split(",")
+      
+   def process(self) -> str:
+      substring_list = self._create_substring()
+      output_val = ""
+      for element in substring_list:
+         for inpt_string in self.input_string_list:
+            if inpt_string.startswith(element):
+               output_val = element
+            else:
+               break
+      return output_val
 
-def get_user_data(self):
-   str_data = []
-   e = 0
-   while e < input_data:
-      val = input("- Enter a word - ")
-      e += 1
-      str_data.append(val)
-   return str_data
+
+   def _create_substring(self) -> list:
+      sorted_list = sorted(self.input_string_list)
+      output_list = []
+      for i, char_ in enumerate(sorted_list[0]):
+         output_list.append(sorted_list[0][:i+1])
+
+      return output_list
+
+def get_user_data(input_data):
+   # str_data = []
+   # e = 0
+   # while e < input_data:
+   #    val = input("- Enter a word - ")
+   #    e += 1
+   #    str_data.append(val)
+   raw_string = ""
 
 
-input_data = int(input("Enter the number of strings "))
-user_data = get_user_data(input_data)
+def process():
+   # 1.loop to get incremental substrings from 1st input string - ex: fall - f, fa ,fal, fall
+   # 2.input is outpt of 1  
+   pass
 
+
+
+input_ = input("Enter strings separated by comma: ")
+prefix_finder = LongestPrefix(input_)
+output = prefix_finder.process()
+print(f"Longest prefix is {output}")
