2530f856d331953bdc1c792df40ea3ec5d75e91f
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-04-26 16:31:08 +0530

    first commit

---
+++ longest_prefix.py
@@ -0,0 +1,25 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+# If there is no common prefix, return an empty string "".
+
+ 
+
+# Example 1:
+
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+
+
+def get_user_data(self):
+   str_data = []
+   e = 0
+   while e < input_data:
+      val = input("- Enter a word - ")
+      e += 1
+      str_data.append(val)
+   return str_data
+
+
+input_data = int(input("Enter the number of strings "))
+user_data = get_user_data(input_data)
+
